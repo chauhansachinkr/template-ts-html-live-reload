f758b45968fdc4418d307435218a6ad9f8f35a40
sachin chauhan <chauhan.sachinkr@gmail.com>
2021-05-08 23:20:42 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Overview
+Basic type script setup with html live reload 
+
+```sh
+npm start
+tsc --watch
+
+```
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+const person: { name: string, age: number } = { name: "sachin", age: 30 }
+
+console.log("Live server reaload is working")
+console.log(person)
