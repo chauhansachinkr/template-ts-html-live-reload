0b20707ca1a21267ca9b5286571c2022999360e9
sachin chauhan <chauhan.sachinkr@gmail.com>
2021-05-09 23:41:42 +0300

    working with webpack

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import {ITDepartment} from './inheritencetest'
+import _ from 'lodash'
 
 const department =new ITDepartment("IT", ['tony', 'stark'], 200)
 department.addEmployee("margetarta!!!")
 
 console.log(department) 
 console.log("This woorks too in new waynewsachin!!!!!!")    
+console.log(_.capitalize("capitalized work"))
